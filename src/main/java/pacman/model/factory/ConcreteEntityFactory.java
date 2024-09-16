ae1d367a7684ad53302eb87c7dd84583b3c411d4
JeslynB9 <jeslyn.bowman@gmail.com>
2024-09-16 17:32:23 +1000

    Drawn to Canvas

--- src/main/java/pacman/model/factory/ConcreteEntityFactory.java
+++ src/main/java/pacman/model/factory/ConcreteEntityFactory.java
@@ -2,15 +2,18 @@ package pacman.model.factory;
 
 import pacman.model.entity.Renderable;
 import pacman.model.entity.dynamic.ghost.GhostImpl;
-import pacman.model.entity.dynamic.physics.BoundingBoxImpl;
-import pacman.model.entity.dynamic.physics.Vector2D;
+import pacman.model.entity.dynamic.ghost.GhostMode;
+import pacman.model.entity.dynamic.physics.*;
+import pacman.model.entity.dynamic.player.PacmanVisual;
 import pacman.model.entity.staticentity.StaticEntityImpl;
 import pacman.model.entity.staticentity.Wall;
 import pacman.model.entity.dynamic.player.Pacman;
 import pacman.model.entity.dynamic.ghost.Ghost;
 import pacman.model.entity.staticentity.collectable.Pellet;
 import javafx.scene.image.Image;
-import pacman.model.entity.dynamic.physics.BoundingBox;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConcreteEntityFactory implements EntityFactory {
 
@@ -24,8 +27,22 @@ public class ConcreteEntityFactory implements EntityFactory {
         Image wallImage4 = new Image(getClass().getResourceAsStream("/maze/walls/upRight.png"));
         Image wallImage5 = new Image(getClass().getResourceAsStream("/maze/walls/downLeft.png"));
         Image wallImage6 = new Image(getClass().getResourceAsStream("/maze/walls/downRight.png"));
-
         Image pellet = new Image(getClass().getResourceAsStream("/maze/pellet.png"));
+        Image ghostImage = new Image(getClass().getResourceAsStream("/maze/ghosts/ghost.png"));
+
+        KinematicState kinematicState = new KinematicStateImpl.KinematicStateBuilder()
+                .setPosition(new Vector2D(x * 16, y * 16))
+                .setSpeed(1.0)
+                .build();
+
+        // Define ghost's corner targets for scatter mode (example coordinates)
+        Vector2D topLeftCorner = new Vector2D(0, 0);
+        Vector2D bottomRightCorner = new Vector2D(160, 160);
+
+        // Define ghost mode and speeds for each mode (example values)
+        Map<GhostMode, Double> speeds = new HashMap<>();
+        speeds.put(GhostMode.CHASE, 1.0);
+        speeds.put(GhostMode.SCATTER, 0.75);
 
 
         switch (type) {
@@ -35,9 +52,48 @@ public class ConcreteEntityFactory implements EntityFactory {
             case '4': return new StaticEntityImpl(boundingBox, Renderable.Layer.FOREGROUND, wallImage4);
             case '5': return new StaticEntityImpl(boundingBox, Renderable.Layer.FOREGROUND, wallImage5);
             case '6': return new StaticEntityImpl(boundingBox, Renderable.Layer.FOREGROUND, wallImage6);
-//            case 'p': return new Pacman(x, y);
-//            case 'g': return new GhostImpl(x, y);
             case '7': return new Pellet(boundingBox, Renderable.Layer.FOREGROUND, pellet, 100);
+            // Add Pacman instantiation
+            case 'p': {
+                // Load Pacman images for different states (up, down, left, right, closed)
+                Map<PacmanVisual, Image> pacmanImages = Map.of(
+                        PacmanVisual.UP, new Image(getClass().getResourceAsStream("/maze/pacman/playerUp.png")),
+                        PacmanVisual.DOWN, new Image(getClass().getResourceAsStream("/maze/pacman/playerDown.png")),
+                        PacmanVisual.LEFT, new Image(getClass().getResourceAsStream("/maze/pacman/playerLeft.png")),
+                        PacmanVisual.RIGHT, new Image(getClass().getResourceAsStream("/maze/pacman/playerRight.png")),
+                        PacmanVisual.CLOSED, new Image(getClass().getResourceAsStream("/maze/pacman/playerClosed.png"))
+                );
+
+                // Create a kinematic state for Pacman
+                Vector2D position = new Vector2D(x * 16, y * 16);
+                KinematicState kinematicStatePacman = new KinematicStateImpl.KinematicStateBuilder()
+                        .setPosition(position)
+                        .setSpeed(2.0)  // Set initial speed
+                        .build();
+
+                // Create the Pacman instance
+                return new Pacman(
+                        pacmanImages.get(PacmanVisual.LEFT), // Default to the left-facing image
+                        pacmanImages,
+                        boundingBox,
+                        kinematicState
+                );
+
+            }
+
+            case 'g':
+                // Create a ghost with an initial mode of SCATTER and target the bottom-right corner
+                Ghost ghost = new GhostImpl(
+                        ghostImage,
+                        boundingBox,
+                        kinematicState,
+                        GhostMode.SCATTER,
+                        bottomRightCorner,
+                        Direction.LEFT
+                );
+                ghost.setSpeeds(speeds);
+                return ghost;
+
             default: return null;
         }
     }
